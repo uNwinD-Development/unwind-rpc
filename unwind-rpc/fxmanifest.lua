2d749657f849dad37eddb4af841623b067ac5efc
uNwinD-Development <88353944+uNwinD-Development@users.noreply.github.com>
2021-11-07 18:47:15 +0000

    Added Main Files

---
+++ unwind-rpc/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author '<uNwinD/>#7067'
+description 'Call back system for np-base'
+version '1.0'
+
+client_scripts {'client/*.lua'}
+server_scripts {'server/*.lua'}
